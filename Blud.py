204d2ebaabe5df83ab53cb2a8bd9e449bf7178bb
vankova_j <julvanko12@gmail.com>
2024-11-05 14:29:20 +0100

    :)

---
+++ Blud.py
@@ -0,0 +1,22 @@
+import math
+
+class Bludiste:
+    def __innit__(self):
+        self.bludiste = Bludiste
+    def je_volno(self, souradnice):
+        (x,y) = souradnice
+        if x < 0 or y < 0 or
+    def get_sirka(self):
+        return len(self.bludiste)
+    def get_vyska(self):
+        return len(self.bludiste)
+    def get_rozmery(self):
+        return self.get_sirka(), self.get_vyska()
+    def get_vychod(self):
+        pass
+
+
+class BludisteView:
+
+
+class BludisteApp:
